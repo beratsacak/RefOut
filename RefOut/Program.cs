fd14c8b28a4082f02083c850052ad805be1182de
berat584 <berat584@BPC08>
2023-12-14 14:04:13 +0300

    Proje dosyası ekle.

---
+++ RefOut/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdSoyAdRefOutKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "Berat";
+            string soyad = "Saçak";
+            int yas = 46;
+            AdSoyad adSoyad = new AdSoyad();
+            adSoyad.BilgiGuncelle(ref ad, ref soyad, ref yas);
+
+
+            Console.WriteLine("Önceki Değerler:");
+            Console.WriteLine("Ad: " + ad);
+            Console.WriteLine("Soyad: " + soyad);
+            Console.WriteLine("Yaş: " + yas);
+
+
+        }
+    }
+    class AdSoyad
+    {
+        public void BilgiGuncelle(ref string ad, ref string soyad, ref int yas)
+        {
+            ad = "SELO";
+            soyad = "DOĞAN";
+            yas = 456;
+        }
+    }
+}
